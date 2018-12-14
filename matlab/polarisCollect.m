5905e7cdb91ea0f95b95c74c1640c85bd10f0f3d
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2018-12-13 20:52:04 -0500

    started GUI, added POSIX/UNIX timestamp to GX() collection

--- matlab/polarisCollect.m
+++ matlab/polarisCollect.m
@@ -5,14 +5,18 @@ SERIAL_TERMINATOR = hex2dec('0D');   % 0x0D = 0d13 = CR
 SERIAL_TIMEOUT    = 0.05;            % [s]
 SERIAL_COM_PORT   = 'COM4';
 
-global frameNums;
-frameNums = [];
+% tool definition files: each row is filename, toolCode
+% tool code: 'D' = dynamic tool; 'S' = static tool; 'B' = button box
+toolDefFiles = {
+    'C:\Users\f002r5k\Dropbox\projects\surg_nav\NDI\Polaris\Tool Definition Files\Medtronic_960_556_V3.rom', 'D';
+    'C:\Users\f002r5k\Dropbox\projects\surg_nav\NDI\Polaris\Tool Definition Files\PassiveProbe3PTS.rom', 'D'
+    };
+numTools = size(toolDefFiles,1);
 
 % reset MATLAB instrument handles just to be safe
 instrreset();
 
 % open COM port using default settings (9600 baud)
-
 fid1 = serial(SERIAL_COM_PORT,'BaudRate',9600,'Timeout',SERIAL_TIMEOUT,'Terminator',SERIAL_TERMINATOR);
 warning off MATLAB:serial:fread:unsuccessfulRead;
 fopen(fid1);
@@ -56,14 +60,10 @@ disp(['< ' polarisGetResponse(fid1)]);
 polarisSendCommand(fid1, 'IRATE:0');
 disp(['< ' polarisGetResponse(fid1)]);
 
-
-%
-toolDefFiles = {
-    'C:\Users\f002r5k\Dropbox\projects\surg_nav\NDI\Polaris\Tool Definition Files\Medtronic_960_556_V3.rom';
-    'C:\Users\f002r5k\Dropbox\projects\surg_nav\NDI\Polaris\Tool Definition Files\PassiveProbe3PTS.rom'
-    };
-for fileIdx = 1:size(toolDefFiles,1)
-    thisToolFile = toolDefFiles{fileIdx};
+% send tool definition files to Polaris
+% tool files can be generated with NDI 6D Architect software
+for fileIdx = 1:numTools
+    thisToolFile = toolDefFiles{fileIdx,1};
     toolFileID = fopen(thisToolFile);
     if( isempty(toolFileID) )
         fclose(fid1);
@@ -71,6 +71,7 @@ for fileIdx = 1:size(toolDefFiles,1)
     end
     disp(['INITIALIZING PORT ' char(64+fileIdx) ' WITH TOOL FILE: ' thisToolFile(max(strfind(thisToolFile,'\'))+1:end)]);
     
+    % read 64-byte clumps from binary file
     [readBytes, numBytes] = fread(toolFileID,64);
     bytePos = 0;
     while (numBytes == 64)
@@ -80,6 +81,8 @@ for fileIdx = 1:size(toolDefFiles,1)
         [readBytes, numBytes] = fread(toolFileID,64);
         bytePos = bytePos + 64;
     end
+    
+    % read any remaining bytes, padding with FF
     if(numBytes > 0)
         str = ['PVWR:' char(64+fileIdx) dec2hex(bytePos,4) reshape(dec2hex(readBytes,2)',1,[]) repmat('FF',1,64-numBytes)];
         polarisSendCommand(fid1,str);
@@ -93,9 +96,8 @@ for fileIdx = 1:size(toolDefFiles,1)
     polarisSendCommand(fid1, ['PINIT:' char(64+fileIdx)]);
     disp(['< ' polarisGetResponse(fid1)]);
     
-    % enable tracking for the tool ... ASSUMING DYNAMIC TOOL 'D' (may want to
-    % change this to allow for static tools or button boxes)
-    polarisSendCommand(fid1, ['PENA:' char(64+fileIdx) 'D']);
+    % enable tracking for the tool
+    polarisSendCommand(fid1, ['PENA:' char(64+fileIdx) toolDefFiles{fileIdx,2}]);
     disp(['< ' polarisGetResponse(fid1)]);
 end
 
@@ -107,36 +109,66 @@ disp(['< ' polarisGetResponse(fid1)]);
 polarisSendCommand(fid1, 'TSTART:');
 disp(['< ' polarisGetResponse(fid1)]);
 
-% query position of tools
+% run tracking
 for i = 1:50
     
+    % query tool positions
     polarisSendCommand(fid1, 'GX:800B');
-    
     thisResp = polarisGetResponse(fid1);
-    disp(['< ' thisResp]);
+    unixtimestamp = posixtime(datetime('now')); % recover with datetime(unixtimestamp,'ConvertFrom','posixtime')
     
-    frameNums(end+1) = hex2dec(thisResp(end-15:end-8));
-
-    pause(1);
+    % thisResp =[
+    %     'DISABLED' char(10) ...
+    %     'DISABLED' char(10) ...
+    %     'DISABLED' char(10) ...
+    %     '00000000000000000000000000000000000000000000' char(10) ...
+    %     '000000000000000000000000' char(10) ...
+    %     '+00367+00613-00731-09947+043218+001124-175562+02835' char(10) ...
+    %     '+05270-05290+03621-05578+011421-004663-179021+00631' char(10) ...
+    %     'DISABLED' char(10) ...
+    %     '003131000000000000000000000003FF00000000003F' char(10) ...
+    %     '000000000000013900000139'];
+    % disp(['< ' thisResp]);
     
+    % extract quaternion, position, timestamp, and estimated error for each tool
+    respParts = strsplit(thisResp,char(10));
+    thisStatusStr = respParts{end};
+    for toolIdx = 1:numTools
+        thisTransformStr = respParts{ 5 + toolIdx };
+        assert( length(thisTransformStr) == 51, 'Unexpected transform string length');
+        q = zeros(4,1);
+        t = zeros(3,1);
+        
+        % extract rotational component (quaternion)
+        for qIdx = 1:4
+            startIdx = (qIdx-1)*6 + 1;
+            q(qIdx) = str2num(thisTransformStr(startIdx:startIdx+5))/10000;
+        end
+        
+        % extract translational component
+        for tIdx = 1:3
+            startIdx = (tIdx-1)*7 + 25;
+            t(tIdx) = str2num(thisTransformStr(startIdx:startIdx+6))/100;
+        end
+        
+        % extract error
+        err = str2num(thisTransformStr(46:end))/10000;
+        
+        % extract timestamp
+        startIdx = (toolIdx-1)*8+1;
+        timestamp = hex2dec(thisStatusStr(startIdx:startIdx+7))/60;
+        
+        % display tool tracking information
+        fprintf('%0.4f,%0.2f,%s,%+0.4f,%+0.4f,%+0.4f,%+0.4f,%+0.2f,%+0.2f,%+0.2f,%+0.4f\n', timestamp, unixtimestamp, char(64+toolIdx), q(1), q(2), q(3), q(4), t(1), t(2), t(3), err);
+    end
 end
-
-
 
 % end tracking
 polarisSendCommand(fid1, 'TSTOP:');
 disp(['< ' polarisGetResponse(fid1)]);
 
-% % change communication back to 9600 baud
-% polarisSendCommand(fid1, 'COMM:00000');
-% disp(['< ' polarisGetResponse(fid1)]);
-
-% % send a serial break to reset Polaris
-% serialbreak(fid1, 1000);
-
 % close communication
 fclose(fid1);
-
 
 end
 
---
+++ matlab/polarisCollectGUI.m
@@ -0,0 +1,231 @@
+function varargout = polarisCollectGUI(varargin)
+% POLARISCOLLECTGUI MATLAB code for polarisCollectGUI.fig
+%      POLARISCOLLECTGUI, by itself, creates a new POLARISCOLLECTGUI or raises the existing
+%      singleton*.
+%
+%      H = POLARISCOLLECTGUI returns the handle to a new POLARISCOLLECTGUI or the handle to
+%      the existing singleton*.
+%
+%      POLARISCOLLECTGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in POLARISCOLLECTGUI.M with the given input arguments.
+%
+%      POLARISCOLLECTGUI('Property','Value',...) creates a new POLARISCOLLECTGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before polarisCollectGUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to polarisCollectGUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help polarisCollectGUI
+
+% Last Modified by GUIDE v2.5 13-Dec-2018 20:40:21
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @polarisCollectGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @polarisCollectGUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before polarisCollectGUI is made visible.
+function polarisCollectGUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to polarisCollectGUI (see VARARGIN)
+
+% Choose default command line output for polarisCollectGUI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes polarisCollectGUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = polarisCollectGUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+msgbox('hi');
+
+
+% --- Executes on selection change in selectToolBox.
+function selectToolBox_Callback(hObject, eventdata, handles)
+% hObject    handle to selectToolBox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns selectToolBox contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from selectToolBox
+contents = cellstr(get(hObject,'String'));
+disp(contents{get(hObject,'Value')});
+
+% --- Executes during object creation, after setting all properties.
+function selectToolBox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to selectToolBox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+set(hObject,'String',num2cell(char(64+(1:9))));
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton8.
+function pushbutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton9.
+function pushbutton9_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
