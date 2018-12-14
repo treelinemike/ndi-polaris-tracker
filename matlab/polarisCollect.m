2b273b4c9eda4ab49bdc88df631e1db9bb1f48d1
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2018-12-14 10:46:56 -0500

    Got bare minimum tracking working from command line / MATLAB IDE. GUI not working yet but we can now collect tracking data from the Polaris.

--- matlab/polarisCollect.m
+++ matlab/polarisCollect.m
@@ -1,17 +1,48 @@
 function polarisCollect
 
-% options
-SERIAL_TERMINATOR = hex2dec('0D');   % 0x0D = 0d13 = CR
-SERIAL_TIMEOUT    = 0.05;            % [s]
-SERIAL_COM_PORT   = 'COM4';
+cleanupHandle = onCleanup(@endTracking);
+% output file descriptor, will be given extensions .csv and .log
+trialID = 'trial';
+outputFilePath = 'C:\Users\f002r5k\GitHub\ndi-polaris-tracker\matlab';
 
 % tool definition files: each row is filename, toolCode
 % tool code: 'D' = dynamic tool; 'S' = static tool; 'B' = button box
 toolDefFiles = {
     'C:\Users\f002r5k\Dropbox\projects\surg_nav\NDI\Polaris\Tool Definition Files\Medtronic_960_556_V3.rom', 'D';
     'C:\Users\f002r5k\Dropbox\projects\surg_nav\NDI\Polaris\Tool Definition Files\PassiveProbe3PTS.rom', 'D'
-    };
-numTools = size(toolDefFiles,1);
+    '', '';
+    '', '';
+    '', '';
+    '', '';
+    '', '';
+    '', '';
+    '', '' };
+% make sure
+if (min(size(toolDefFiles) == [9,2]) == 0)
+    error('Incorrect tool file definition cell array size.')
+end
+
+% determine which tools are used and format GX() calls appropriately
+toolsUsedMask = ~cellfun(@isempty,toolDefFiles(:,1));
+toolsUsed = find(toolsUsedMask);
+if( max(toolsUsed) < 4 )
+    extended_flag = 0;
+    gx_cmd_str = 'GX:800B';
+    pstat_cmd_str = 'PSTAT:801f';
+else
+    extended_flag = 1;
+    gx_cmd_str = 'GX:A00B';
+    pstat_cmd_str = 'PSTAT:A01f';
+end
+
+% options
+SERIAL_TERMINATOR = hex2dec('0D');   % 0x0D = 0d13 = CR
+SERIAL_TIMEOUT    = 0.05;            % [s]
+SERIAL_COM_PORT   = 'COM4';
+BASE_TOOL_CHAR = 64;
+
+% gx transform mapping
+gx_transform_map = [6 7 8 10 11 12 14 15 16];
 
 % reset MATLAB instrument handles just to be safe
 instrreset();
@@ -29,11 +60,11 @@ disp(['< ' polarisGetResponse(fid1)]);
 pause(1);
 
 % produce audible beep as confirmation
-polarisSendCommand(fid1, 'BEEP:1');
+polarisSendCommand(fid1, 'BEEP:1',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % change communication to 57,600 baud
-polarisSendCommand(fid1, 'COMM:40000');
+polarisSendCommand(fid1, 'COMM:40000',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % swich MATLAB COM port settings to 57,600 baud
@@ -45,38 +76,39 @@ warning off MATLAB:serial:fread:unsuccessfulRead;
 fopen(fid1);
 
 % produce audible beeps as confimation
-polarisSendCommand(fid1, 'BEEP:2');
+polarisSendCommand(fid1, 'BEEP:2',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % initialize system
-polarisSendCommand(fid1, 'INIT:');
+polarisSendCommand(fid1, 'INIT:',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % select volume (doing this blindly without querying volumes first)
-polarisSendCommand(fid1, 'VSEL:1');
+polarisSendCommand(fid1, 'VSEL:1',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % set illuminator rate to 20Hz
-polarisSendCommand(fid1, 'IRATE:0');
+polarisSendCommand(fid1, 'IRATE:0',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % send tool definition files to Polaris
 % tool files can be generated with NDI 6D Architect software
-for fileIdx = 1:numTools
-    thisToolFile = toolDefFiles{fileIdx,1};
+for toolIdx = 1:length(toolsUsed)
+    toolNum = toolsUsed(toolIdx);
+    thisToolFile = toolDefFiles{toolNum,1};
     toolFileID = fopen(thisToolFile);
     if( isempty(toolFileID) )
         fclose(fid1);
         error('Invalid tool file and/or path.');
     end
-    disp(['INITIALIZING PORT ' char(64+fileIdx) ' WITH TOOL FILE: ' thisToolFile(max(strfind(thisToolFile,'\'))+1:end)]);
+    disp(['INITIALIZING PORT ' char(BASE_TOOL_CHAR+toolNum) ' WITH TOOL FILE: ' thisToolFile(max(strfind(thisToolFile,'\'))+1:end)]);
     
     % read 64-byte clumps from binary file
     [readBytes, numBytes] = fread(toolFileID,64);
     bytePos = 0;
     while (numBytes == 64)
-        str = ['PVWR:' char(64+fileIdx) dec2hex(bytePos,4) reshape(dec2hex(readBytes,2)',1,[])];
-        polarisSendCommand(fid1,str);
+        str = ['PVWR:' char(BASE_TOOL_CHAR+toolNum) dec2hex(bytePos,4) reshape(dec2hex(readBytes,2)',1,[])];
+        polarisSendCommand(fid1,str,1);
         disp(['< ' polarisGetResponse(fid1)]);
         [readBytes, numBytes] = fread(toolFileID,64);
         bytePos = bytePos + 64;
@@ -84,8 +116,8 @@ for fileIdx = 1:numTools
     
     % read any remaining bytes, padding with FF
     if(numBytes > 0)
-        str = ['PVWR:' char(64+fileIdx) dec2hex(bytePos,4) reshape(dec2hex(readBytes,2)',1,[]) repmat('FF',1,64-numBytes)];
-        polarisSendCommand(fid1,str);
+        str = ['PVWR:' char(BASE_TOOL_CHAR+toolNum) dec2hex(bytePos,4) reshape(dec2hex(readBytes,2)',1,[]) repmat('FF',1,64-numBytes)];
+        polarisSendCommand(fid1,str,1);
         disp(['< ' polarisGetResponse(fid1)]);
     end
     
@@ -93,28 +125,41 @@ for fileIdx = 1:numTools
     fclose(toolFileID);
     
     % initialize port handle for the tool
-    polarisSendCommand(fid1, ['PINIT:' char(64+fileIdx)]);
+    polarisSendCommand(fid1, ['PINIT:' char(BASE_TOOL_CHAR+toolNum)],1);
     disp(['< ' polarisGetResponse(fid1)]);
     
     % enable tracking for the tool
-    polarisSendCommand(fid1, ['PENA:' char(64+fileIdx) toolDefFiles{fileIdx,2}]);
+    polarisSendCommand(fid1, ['PENA:' char(BASE_TOOL_CHAR+toolNum) toolDefFiles{toolNum,2}],1);
     disp(['< ' polarisGetResponse(fid1)]);
 end
 
 % confirm tool configuration
-polarisSendCommand(fid1, 'PSTAT:801f');
+polarisSendCommand(fid1, pstat_cmd_str,1);
 disp(['< ' polarisGetResponse(fid1)]);
 
 % enter tracking mode
-polarisSendCommand(fid1, 'TSTART:');
+polarisSendCommand(fid1, 'TSTART:',1);
 disp(['< ' polarisGetResponse(fid1)]);
 
-% run tracking
-for i = 1:50
+% get output file ready to go
+counter = 1;
+outputFileName = [outputFilePath '\' trialID sprintf('%03d',counter) '.csv'];
+while( isfile(outputFileName))
+    counter = counter + 1;
+    outputFileName = [outputFileName(1:end-7) sprintf('%03d',counter) '.csv'];
+end
+fidDataOut = fopen(outputFileName,'w');
+
+% run tracking, escape with CTRL-C
+while 1
     
     % query tool positions
-    polarisSendCommand(fid1, 'GX:800B');
+    polarisSendCommand(fid1, gx_cmd_str);
     thisResp = polarisGetResponse(fid1);
+    %disp(['< ' thisResp]);
+    
+    % get a unix timestamp for sanity check and future use (may want the
+    % actual DATE)
     unixtimestamp = posixtime(datetime('now')); % recover with datetime(unixtimestamp,'ConvertFrom','posixtime')
     
     % thisResp =[
@@ -133,50 +178,63 @@ for i = 1:50
     % extract quaternion, position, timestamp, and estimated error for each tool
     respParts = strsplit(thisResp,char(10));
     thisStatusStr = respParts{end};
-    for toolIdx = 1:numTools
-        thisTransformStr = respParts{ 5 + toolIdx };
-        assert( length(thisTransformStr) == 51, 'Unexpected transform string length');
-        q = zeros(4,1);
-        t = zeros(3,1);
-        
-        % extract rotational component (quaternion)
-        for qIdx = 1:4
-            startIdx = (qIdx-1)*6 + 1;
-            q(qIdx) = str2num(thisTransformStr(startIdx:startIdx+5))/10000;
+    for toolIdx = 1:length(toolsUsed)
+        toolNum = toolsUsed(toolIdx);
+        thisTransformStr = respParts{ gx_transform_map(toolNum) };
+        if(length(thisTransformStr) == 51)
+            q = zeros(4,1);
+            t = zeros(3,1);
+            
+            % extract rotational component (quaternion)
+            for qIdx = 1:4
+                startIdx = (qIdx-1)*6 + 1;
+                q(qIdx) = str2num(thisTransformStr(startIdx:startIdx+5))/10000;
+            end
+            
+            % extract translational component
+            for tIdx = 1:3
+                startIdx = (tIdx-1)*7 + 25;
+                t(tIdx) = str2num(thisTransformStr(startIdx:startIdx+6))/100;
+            end
+            
+            % extract error
+            err = str2num(thisTransformStr(46:end))/10000;
+            
+            % extract timestamp
+            startIdx = (toolNum-1)*8+1;
+            timestamp = hex2dec(thisStatusStr(startIdx:startIdx+7))/60;
+            
+            % display tool tracking information
+            fprintf('%0.4f,%0.2f,%s,%+0.4f,%+0.4f,%+0.4f,%+0.4f,%+0.2f,%+0.2f,%+0.2f,%+0.4f\n', timestamp, unixtimestamp, char(BASE_TOOL_CHAR+toolNum), q(1), q(2), q(3), q(4), t(1), t(2), t(3), err);
+            fprintf(fidDataOut,'%0.4f,%0.2f,%s,%+0.4f,%+0.4f,%+0.4f,%+0.4f,%+0.2f,%+0.2f,%+0.2f,%+0.4f\n', timestamp, unixtimestamp, char(BASE_TOOL_CHAR+toolNum), q(1), q(2), q(3), q(4), t(1), t(2), t(3), err);
+        else
+            disp(['Tool ' char(BASE_TOOL_CHAR+toolNum) ' unexpected transform result: ' thisTransformStr]);
         end
-        
-        % extract translational component
-        for tIdx = 1:3
-            startIdx = (tIdx-1)*7 + 25;
-            t(tIdx) = str2num(thisTransformStr(startIdx:startIdx+6))/100;
-        end
-        
-        % extract error
-        err = str2num(thisTransformStr(46:end))/10000;
-        
-        % extract timestamp
-        startIdx = (toolIdx-1)*8+1;
-        timestamp = hex2dec(thisStatusStr(startIdx:startIdx+7))/60;
-        
-        % display tool tracking information
-        fprintf('%0.4f,%0.2f,%s,%+0.4f,%+0.4f,%+0.4f,%+0.4f,%+0.2f,%+0.2f,%+0.2f,%+0.4f\n', timestamp, unixtimestamp, char(64+toolIdx), q(1), q(2), q(3), q(4), t(1), t(2), t(3), err);
     end
 end
 
-% end tracking
-polarisSendCommand(fid1, 'TSTOP:');
-disp(['< ' polarisGetResponse(fid1)]);
-
-% close communication
-fclose(fid1);
+    function endTracking()
+        % close output file
+        fclose(fidDataOut);
+        
+        % end tracking
+        polarisSendCommand(fid1, 'TSTOP:',1);
+        disp(['< ' polarisGetResponse(fid1)]);
+        
+        % close communication
+        fclose(fid1);
+    end
 
 end
 
-function polarisSendCommand(comPortHandle, cmdStr)
+function polarisSendCommand(comPortHandle, cmdStr, varargin)
 
 realStr = [cmdStr polarisCRC16(0,cmdStr)];
 fprintf(comPortHandle,realStr); % note: terminator added automatically (default fprintf format is %s\n
-disp(['> ' strtrim(cmdStr)]);
+
+if(nargin > 2 && varargin{1} == 1)
+    disp(['> ' strtrim(cmdStr)]);
+end
 
 end
 
