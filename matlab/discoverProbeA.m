8715965e8a6fc84f33c3a32bafb42e203dfd6805
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2019-03-13 13:38:38 -0400

    fixed eigenvector adjustment scheme and confirmed global and local coordinate systems. achieved 0.3484 mm RMSE between estimated marker locations and "actual" locations from tool definition file proved by Xiaoyao Fan on 27-NOV-18

--- matlab/discoverProbeA.m
+++ matlab/discoverProbeA.m
@@ -2,7 +2,7 @@
 close all; clear all; clc;
 
 % options
-doShowPlots = 0;
+doShowAllPlots = 0;
 
 % load data
 allData = load('medtronic_data_1_identify.csv');
@@ -22,67 +22,63 @@ for rowIdx = 1:size(allData,1)
     
     % rearrange eigenvectors in order of decreasing eigenvalues
     % so x axis has the largest variation, and z axis has the least
-    [valSort valSortIdx] = sort(diag(val),'descend');
+    [valSort, valSortIdx] = sort(diag(val),'descend');
     vecSort = vec(:,valSortIdx);
     
-    localPts = (vecSort')*relLocs(:,relDistSortIdx);
-    
-    % flip x axis if not pointing toward the closest point along
-    if (min(abs(localPts(1,(localPts(1,:)<0)))) > min(localPts(1,(localPts(1,:)>0))))
+    % make sure that eigenvectors are pointing in generally the right
+    % directions
+    % assumptions:
+    %   - array on pointer device is taller than it is wide
+    %   - x axis along the eigenvector with the largest eigenvalue, positive generally aligned with the tracker NEGATIVE x axis (points away from tip)
+    %   - z axis along the eigenvector with the smallest eigenvalue, positive pointing generally toward tracker head
+    if(vecSort(1,1) > 0)
         vecSort = vecSort*diag([-1 1 1]);
     end
-    
-    % flip y axis if not pointing toward the closest point along
-    if (min(abs(localPts(2,(localPts(2,:)<0)))) > min(localPts(2,(localPts(2,:)>0))))
-        vecSort = vecSort*diag([1 -1 1]);
+    if(vecSort(3,3) < 0)
+        vecSort = vecSort*diag([1 1 -1]);
     end
     
-    % make sure z axis is consistent (right-handed CS)
-    vecSort(:,3) = cross(vecSort(:,1),vecSort(:,2));
+    % make sure y axis is consistent (right-handed CS)
+    vecSort(:,2) = cross(vecSort(:,3),vecSort(:,1));
     
-    % recalculate local points with adjusted coordinate system
+    % transform data into local CS
     localPts = (vecSort')*relLocs(:,relDistSortIdx);
     
+    % store local data
     allRelXYZ(end+1,:) = reshape(localPts,1,[]);
     
-    if(doShowPlots)
+    % show each frame if desired
+    if(doShowAllPlots)
         hold off;
-        %     plot3(sortedLocs(1,:),sortedLocs(2,:),sortedLocs(3,:),'.','MarkerSize',25);
-        for locIdx = 1:size(sortedLocs,2)
-            %         plot3(sortedLocs(1,locIdx),sortedLocs(2,locIdx),sortedLocs(3,locIdx),'.','MarkerSize',25);
-            plot3(localPts(1,locIdx),localPts(2,locIdx),localPts(3,locIdx),'.','MarkerSize',25);
-            
+        for i = 1:3:size(allRelXYZ,2)
+            plot3(allRelXYZ(:,i),allRelXYZ(:,i+1),allRelXYZ(:,i+2),'k.','MarkerSize',10);
             hold on;
         end
-        %     vec = vec*100;
-        %     plot3(meanLoc(1),meanLoc(2),meanLoc(3),'k*','MarkerSize',10);
-        %     plot3(meanLoc(1)+[0 vec(1,1)],meanLoc(2)+[0 vec(2,1)],meanLoc(3)+[0 vec(3,1)],'r-');
-        %     plot3(meanLoc(1)+[0 vec(1,2)],meanLoc(2)+[0 vec(2,2)],meanLoc(3)+[0 vec(3,2)],'g-');
-        %     plot3(meanLoc(1)+[0 vec(1,3)],meanLoc(2)+[0 vec(2,3)],meanLoc(3)+[0 vec(3,3)],'b-');
-        
+
+        for locIdx = 1:size(sortedLocs,2) % in a loop for colors...
+            plot3(localPts(1,locIdx),localPts(2,locIdx),localPts(3,locIdx),'.','MarkerSize',25);
+        end
         
         grid on;
-        %     view([100,50]);
-        %         view([128,10]);
-        view([90,0]);
+        
+        view([270,90]);
         axis equal;
         xlabel('x');
         ylabel('y');
         zlabel('z');
-        %     xlim([-400,0]);
-        %     ylim([-200,200]);
-        %     zlim([-2100,-1800]);
-        
         xlim([-200,200]);
         ylim([-200,200]);
         zlim([-200,200]);
         
-        pause(0.1);
+        drawnow;
+%         pause(0.1);
     end
 end
 
 
-figure; hold on; grid on;
+% show all data overlaid in local CS
+figure;
+hold on; grid on;
 for i = 1:3:size(allRelXYZ,2)
     plot3(allRelXYZ(:,i),allRelXYZ(:,i+1),allRelXYZ(:,i+2),'.','MarkerSize',15);
 end
@@ -94,14 +90,27 @@ view([130,34]);
 xlim([-200,200]);
 ylim([-200,200]);
 zlim([-200,200]);
-estimate = reshape(mean(allRelXYZ),3,[])'
-groundTruth = [160,0,0;230,0,0;305,0,0;263.67,-43.49,0;256,42.71,0]
-[tform,movingReg,rmse] = pcregistericp(pointCloud(estimate),pointCloud(groundTruth));
-plot3(movingReg.Location(:,1),movingReg.Location(:,2),movingReg.Location(:,3))
 
-[~,sidx] = sort(estimate(:,1));
-est2 = estimate(sidx,:) 
-est2 = est2 - repmat([est2(1,1) 0 0],5,1)
-[~,sidx] = sort(groundTruth(:,1));
-gt2 = groundTruth(sidx,:);
-gt2 = gt2 - repmat([gt2(1,1) 0 0],5,1)
+% compute estimated marker locations relative to centroid
+% in local coordinate system
+% for now we just average the marker locations over all available data
+% but in the future an optimization routine might be more accurate
+estimate = reshape(mean(allRelXYZ),3,[])'
+
+% ground truth, relative to centroid
+groundTruth = [160,0,0;230,0,0;305,0,0;263.67,-43.49,0;256,42.71,0];  % from Xiaoyao
+groundTruth = groundTruth-repmat(mean(groundTruth),size(groundTruth,1),1);
+
+% align estimate with ground truth to see how close we came (measured by RMSE)
+[tform,movingReg,rmse] = pcregistericp(pointCloud(estimate),pointCloud(groundTruth));%,'InitialTransform',affine3d([-1 0 0 0; 0 -1 0 0; 0 0 1 0; 0 0 0 1]));
+rmse
+
+% plot results
+plot3(movingReg.Location(:,1),movingReg.Location(:,2),movingReg.Location(:,3),'r+','MarkerSize',5);
+plot3(groundTruth(:,1),groundTruth(:,2),groundTruth(:,3),'bo','MarkerSize',5);
+% [~,sidx] = sort(estimate(:,1));
+% est2 = estimate(sidx,:)
+% est2 = est2 - repmat([est2(1,1) 0 0],5,1)
+% [~,sidx] = sort(groundTruth(:,1));
+% gt2 = groundTruth(sidx,:);
+% gt2 = gt2 - repmat([gt2(1,1) 0 0],5,1)
